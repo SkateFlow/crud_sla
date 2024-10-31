9d008889572916428a1b040c9c033f915fe1071c
anafonseca05 <a.luizamedeiros05@gmail.com>
2024-10-31 20:29:29 -0300

    Atualizado3

--- servidor/sla/src/main/java/com/itb/inf2e/sla/config/CorsConfiguration.java
+++ servidor/sla/src/main/java/com/itb/inf2e/sla/config/CorsConfiguration.java
@@ -10,7 +10,7 @@ public class CorsConfiguration implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:8080") // URL do front-end permitido
+                .allowedOrigins("http://localhost:8080", "http://localhost:5173") // URL do front-end permitido
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT");
     }
 }
