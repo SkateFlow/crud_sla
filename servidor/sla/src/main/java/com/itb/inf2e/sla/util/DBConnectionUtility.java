7ad5f69b936b2dbb20257ccc37c6a967a3ec55dc
anafonseca05 <a.luizamedeiros05@gmail.com>
2024-10-31 07:56:44 -0300

    Atualizado2

--- servidor/sla/src/main/java/com/itb/inf2e/sla/util/DBConnectionUtility.java
+++ servidor/sla/src/main/java/com/itb/inf2e/sla/util/DBConnectionUtility.java
@@ -8,8 +8,8 @@ import java.sql.SQLException;
 public class DBConnectionUtility {
 
     private static final String JDBC_URL = "jdbc:sqlserver://localhost:1433;databaseName=bd_grupo3_2em";
-    private static final String USER = "seu_usuario"; // substitua pelo seu usuário
-    private static final String PASSWORD = "sua_senha"; // substitua pela sua senha
+    private static final String USER = "sa"; // substitua pelo seu usuário
+    private static final String PASSWORD = "@ITB123456"; // substitua pela sua senha
 
     // Método para obter a conexão com o banco de dados
     public static Connection getDBConnection() throws SQLException {
